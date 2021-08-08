63a0489ee68bd38b668102b2ec620d6e7053643a
Ismail Mum <ismail.mum@siemens.com>
2021-08-08 19:10:58 +0300

    deletion of unused code parts

--- utils/mongo_utils.py
+++ utils/mongo_utils.py
@@ -1,33 +1,5 @@
 import pymongo
 
-
-class mongoDB():
-    def __init__ (self, database = 'Inventory'):
-    #TODO: Catch connection errors
-        self.connection = pymongo.MongoClient("mongodb://localhost:27016/")
-        self.db = self.connection[database]
-
-    # Methods for 'Master' collection
-    def insert_master(self, elements):
-        insert = self.db["Master"].insert_many(elements)
-        return insert.inserted_ids
-
-    def find_master(self):
-        return list(self.db['Master'].find())
-
-    # Methods for 'Counts' collection
-    def insert_counts(self, elements):
-        insert = self.db["Counts"].insert_many(elements)
-        return insert.inserted_ids
-
-    def find_counts(self):
-        return list(self.db['Counts'].find())
-
-    def __del__(self):
-        self.connection.close()
-        print("db connection closed..")
-
-
 # Returns MongoDB connection to 'Inventory' database
 def create_db_connection():
     return pymongo.MongoClient("mongodb://localhost:27017/")["Inventory"]
@@ -35,7 +7,6 @@ def create_db_connection():
 
 # Methods for 'Master' collection
 def insert_master(db_connection, elements):
-#    inventory_db = db_connection["Inventory"]
     insert = db_connection["Master"].insert_many(elements)
     return insert.inserted_ids
 
@@ -48,7 +19,6 @@ def find_master(db_connection):
 
 # Methods for 'Counts' collection
 def insert_counts(db_connection, elements):
-#    inventory_db = db_connection["Inventory"]
     insert = db_connection["Counts"].insert_many(elements)
     return insert.inserted_ids
 
